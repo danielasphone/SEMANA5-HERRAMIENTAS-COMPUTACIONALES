7074b2d588bc889c380bbd9df7fa235a0775ee0d
danielasphone <148790373+danielasphone@users.noreply.github.com>
2024-09-11 01:47:14 +0000

    Comentarios añadidos paint (CORRECION)

--- paint.py
+++ paint.py
@@ -21,20 +21,24 @@ def square(start, end):
 
     end_fill()
 
+#Se renombro funcion "circle" a "draw_circle" para evitar conflictos con la funcion "circle" de la biblioteca turtle
+#Calcula radio basandose en la distancia entre start y end y utiliza funcion circle de turtle para dibujarlo
 def draw_circle(start, end):
     "Draw circle from start to end."
-    # Calcula radio basandose en distancia entre start y end
+    # Calcula radio 
     radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2) ** 0.5
     
     up()
-    # Ve al punto donde deberia estar el centro del circulo y ajusta
+    # ajustamos punto inicial para dibujar circulo 
     goto(start.x, start.y - radius)
     down()
     
     begin_fill()
-    circle(radius)  # para usar circle de turtle para dibujar el circulo
+    circle(radius)  # usar circle de turtle para dibujar el circulo
     end_fill()
 
+#Utilizamos punto "start" como una esquina y "end" como esquina opuesta 
+#Se crean 4 lados usando un bucle 
 def rectangle(start, end):
     "Draw rectangle from start to end."
     up()  # Levanta el lapiz
@@ -51,6 +55,7 @@ def rectangle(start, end):
 
     end_fill()
 
+#Usa punto "start" como primer vertice y "end" para definir base, luego se completan los 3 lados
 def triangle(start, end):
     "Draw triangle from start to end."
     up()  # Levanta el lapiz
@@ -62,19 +67,20 @@ def triangle(start, end):
     for count in range(3): 
         forward(end.x - start.x) # Dibuja un lado del triangulo
         left(120) # Gira 120 grados a la izquierda para formar el angulo del triangulo
-        
+
     end_fill() # Termina de rellenar el triangulo
 
+#Determina si el usuario esta comenzando una nueva figura o si ya termino de definir la forma 
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
 
     if start is None:
-        state['start'] = vector(x, y)
+        state['start'] = vector(x, y) # Almacena punto inicio 
     else:
         shape = state['shape'] # Obtiene la forma que se va a dibujar
         end = vector(x, y)
-        shape(start, end)
+        shape(start, end) #Llama funcion de figura seleccionada 
         state['start'] = None # Resetea el punto de inicio despues de dibujar
 
 def store(key, value):
@@ -85,7 +91,7 @@ def store(key, value):
 state = {'start': None, 'shape': line}
 #Tamaño de la ventana de dibujo
 setup(420, 420, 370, 0)
-# Detecta clics en pantalla para iniciar el dibujo
+# Detecta clics en pantalla para iniciar el dibujo, asigna funcion tap a clic mouse
 onscreenclick(tap)
 listen()
 
