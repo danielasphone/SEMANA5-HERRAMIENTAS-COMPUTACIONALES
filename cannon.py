35121fc21ba53bc533a76062244e83112baa0869
PolouX <deoh02@gmail.com>
2024-09-10 23:48:15 -0600

    Hacer juego infinito

--- cannon.py
+++ cannon.py
@@ -43,6 +43,9 @@ def move():
     for target in targets:
         target.x -= 1.5 #velocidad balones, mayor mas velocidad 
 
+        if not inside(target): #si sale de la pantalla ajustar posicion al final de la pantalla
+            target.x = 200
+
     if inside(ball):
         speed.y -= 0.35
         ball.move(speed)
@@ -55,10 +58,6 @@ def move():
             targets.append(target)
 
     draw()
-
-    for target in targets:
-        if not inside(target):
-            return
 
     ontimer(move, 50)
 
